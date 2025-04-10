5ecb21243f081beb3d9bc39bc347e990e9761f99
Liam <liamclyde.2001@gmail.com>
2025-04-10 22:11:46 +0530

    demonstrate(branching): shrey branch

--- something.c
+++ something.c
@@ -1,4 +1,5 @@
 void main() {
     printf("This is an example I pushed through local repository");
     printf("This was pushed through the remote directly");
+    printf("I am pushing this into shrey branch only.");
 }
