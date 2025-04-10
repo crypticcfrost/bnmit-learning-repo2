34907e040ac42c1ac8a29e2136150022cad4a1e3
Liam <liamclyde.2001@gmail.com>
2025-04-10 22:22:00 +0530

    delete

--- something.c
+++
@@ -1,5 +0,0 @@
-void main() {
-    printf("This is an example I pushed through local repository");
-    printf("This was pushed through the remote directly");
-    printf("I am pushing this into shrey branch only.");
-}
